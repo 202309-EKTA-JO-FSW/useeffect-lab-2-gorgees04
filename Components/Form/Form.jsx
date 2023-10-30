c96091f8e1ee2b2c7c63f3e254f65774035b8cb3
Gorgees Odisho <gorgees041@gmail.com>
2023-10-31 01:35:36 +0300

    activity done

--- Components/Form/Form.jsx
+++ Components/Form/Form.jsx
@@ -1,10 +1,18 @@
-export default function Form() {
+import { useEffect, useState } from "react";
+
+export default function Form({ onNameChange, name }) {
   return (
     <form>
       <h3>Search user Github</h3>
       <div className="mb-3">
         <label>User name</label>
-        <input type="text" className="form-control" placeholder="User Name" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="User Name"
+          value={name}
+          onChange={onNameChange}
+        />
       </div>
     </form>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,61 @@
-import Form from '../Components/Form/Form'
-import UserCard from '../Components/UserCard/UserCard'
-import { Inter } from 'next/font/google'
+import { useState, useEffect } from "react";
+import Form from "../Components/Form/Form";
+import UserCard from "../Components/UserCard/UserCard";
+import { Inter } from "next/font/google";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.github.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setUsers(users);
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (name) {
+      fetch(`https://api.github.com/users/${name}`)
+        .then((response) => response.json())
+        .then((user) => {
+          console.log(user);
+          setUsers([user]);
+        })
+        .catch((error) => {
+          console.error("Error fetching user data: " + error);
+        });
+    } else {
+      fetch("https://api.github.com/users")
+        .then((response) => response.json())
+        .then((user) => {
+          console.log(user);
+          setUsers(user);
+        })
+        .catch((error) => {
+          console.error("Error fetching user data: " + error);
+        });
+    }
+  }, [name]);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  // handle changes
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div className="card">
-      <Form />
-      <UserCard />
+      <Form onNameChange={handleChangeName} name={name} />
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
