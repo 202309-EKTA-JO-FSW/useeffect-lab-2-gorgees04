24ef009ab683c5917f025be825fd2bdb2d5d6001
Gorgees Odisho <gorgees041@gmail.com>
2023-11-05 01:56:26 +0300

    useEffect activity 2 done

--- Components/Form/Form.jsx
+++ Components/Form/Form.jsx
@@ -1,6 +1,30 @@
 import { useEffect, useState } from "react";
 
-export default function Form({ onNameChange, name }) {
+export default function Form({ onChangeName }) {
+  const [name, setName] = useState("");
+  useEffect(() => {
+    const fetchhData = async () => {
+      const res = await fetch(`https://api.github.com/users`);
+      const data = await res.json();
+      if (name.length > 0) {
+        const filteredData = data.filter((data) => data.login.startsWith(name));
+        onChangeName(filteredData);
+      } else {
+        onChangeName(data);
+      }
+    };
+    fetchhData();
+  }, [name]);
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  if (name > 0) {
+    const filteredData = nameData.filter((data) => data.login.startsWith(name));
+    onChangeName(filteredData);
+  }
+
   return (
     <form>
       <h3>Search user Github</h3>
@@ -11,7 +35,7 @@ export default function Form({ onNameChange, name }) {
           className="form-control"
           placeholder="User Name"
           value={name}
-          onChange={onNameChange}
+          onChange={handleChangeName}
         />
       </div>
     </form>
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   const [users, setUsers] = useState([]);
   const [isLoading, setLoading] = useState(true);
-  const [name, setName] = useState("");
 
   useEffect(() => {
     fetch("https://api.github.com/users")
@@ -19,43 +18,21 @@ export default function Home() {
       });
   }, []);
 
-  useEffect(() => {
-    if (name) {
-      fetch(`https://api.github.com/users/${name}`)
-        .then((response) => response.json())
-        .then((user) => {
-          console.log(user);
-          setUsers([user]);
-        })
-        .catch((error) => {
-          console.error("Error fetching user data: " + error);
-        });
-    } else {
-      fetch("https://api.github.com/users")
-        .then((response) => response.json())
-        .then((user) => {
-          console.log(user);
-          setUsers(user);
-        })
-        .catch((error) => {
-          console.error("Error fetching user data: " + error);
-        });
-    }
-  }, [name]);
-
   if (isLoading) return <p>Loading...</p>;
 
   // handle changes
-  const handleChangeName = (e) => {
-    setName(e.target.value);
+  const onChangeName = (name) => {
+    setUsers(name);
   };
-
+  console.log(users);
   return (
-    <div className="card">
-      <Form onNameChange={handleChangeName} name={name} />
-      {users.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
+    <div className={`card ${inter.className}`}>
+      <Form onChangeName={onChangeName} />
+      {users.length > 0 ? (
+        users.map((user) => <UserCard key={user.id} user={user} />)
+      ) : (
+        <h1>Not Found</h1>
+      )}
     </div>
   );
 }
